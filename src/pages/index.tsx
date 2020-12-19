1079fbc0f00020a1cdad3dbb35f7ceeeb68b3df0
Daniel Noyola <daniel.noyola.dev@gmail.com>
2020-12-18 19:04:03 -0600

    index query.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { graphql, PageProps } from "gatsby";
+import { SanityPost, SanityPostConnection, SanityPostEdge } from "../types";
 
-function IndexPage() {
-  return <h1>Index page</h1>;
+interface IndexPageProps extends PageProps {
+  data: {
+    posts: SanityPostConnection;
+  };
 }
+
+function IndexPage({ data }: IndexPageProps) {
+  return (
+    <>
+      <h1>Daniel Noyola Blog re-design</h1>
+      {data.posts.nodes.map((post) => (
+        <h3 key={post.id}>{post.title}</h3>
+      ))}
+    </>
+  );
+}
+
+export const query = graphql`
+  query indexPageQuery {
+    posts: allSanityPost {
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`;
 
 export default IndexPage;
