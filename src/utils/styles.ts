fa3021bc37916745344c09ef50eb97a1205fb3c5
Daniel Noyola <daniel.noyola.dev@gmail.com>
2020-12-24 10:14:02 -0600

    Styles

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -4,20 +4,27 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --font-primary: 'Fira Sans', sans-serif;
     --font-secondary: 'Libre Baskerville', serif;
+    --color-neutral-98: #f7f8fa;
+    --color-neutral-90: #e1e3e6;
+    --color-neutral-80: #c4c7cc;
+    --color-neutral-60: #909499;
+    --color-neutral-25: #3b3d40;
+    --color-neutral-15: #222426;
+    
     --color-primary: #D95736;
     --color-primary-active: #BF4526;
     --color-text-primary: #222426;
     --color-text-secondary: #909499;
-    --color-white: #FFF;
-    --color-neutral-25: #3b3d40;
+    --color-white: #ffffff;
+    
   }
-  @media (prefers-color-scheme: dark) {
-		--color-white: #222426;
-		--color-text-primary: #ffffff;
-	}
   html {
     font-size: 62.5%;
     overflow-x: hidden;
+    @media (prefers-color-scheme: dark) {
+      --color-white: #222426;
+      --color-text-primary: #ffffff;
+    }
   }
   .menu-active body,
   html.menu-active {
