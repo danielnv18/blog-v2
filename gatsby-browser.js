4e53b2418ca68dfa616d30991def8de2721148bf
Daniel Noyola <daniel.noyola.dev@gmail.com>
2021-01-07 17:23:55 -0600

    normalize.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { Normalize } from "styled-normalize";
+import "normalize.css";
 import { GlobalStyle, theme } from "./src/utils/styles";
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
-    <Normalize />
     <GlobalStyle />
     {element}
   </ThemeProvider>
