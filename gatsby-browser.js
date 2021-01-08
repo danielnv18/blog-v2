3214d6ee84a428c360310e7f93b9dd93a5f8224a
Daniel Noyola <daniel.noyola.dev@gmail.com>
2021-01-07 21:13:59 -0600

    another test.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import "normalize.css";
+import "./src/utils/styles.css";
 import { GlobalStyle, theme } from "./src/utils/styles";
 
 export const wrapRootElement = ({ element }) => (
--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,49 +1,6 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  :root {
-    --font-primary: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    --font-secondary: Georgia,serif;
-    --color-neutral-98: #f7f8fa;
-    --color-neutral-90: #e1e3e6;
-    --color-neutral-80: #c4c7cc;
-    --color-neutral-60: #909499;
-    --color-neutral-25: #3b3d40;
-    --color-neutral-15: #222426;
-    
-    --color-primary: #D95736;
-    --color-primary-active: #BF4526;
-    --color-text-primary: #222426;
-    --color-text-secondary: #909499;
-    --color-white: #ffffff;
-    
-  }
-  html {
-    font-size: 62.5%;
-    overflow-x: hidden;
-    @media (prefers-color-scheme: dark) {
-      --color-white: #222426;
-      --color-text-primary: #ffffff;
-    }
-  }
-  .menu-active body,
-  html.menu-active {
-    overflow-y: hidden;
-  }
-  body {
-    font-family: var(--font-primary);
-    font-size: 1em;
-    line-height: 1.5em;
-    font-weight: 400;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    overflow-x: hidden;
-    color: var(--color-text-primary);
-    background: var(--color-white);
-    
-  }
-  
   .menu-active {
     .nav-close {
       @media only screen and (max-width: ${(props) => theme.breakpointMedium}) {
