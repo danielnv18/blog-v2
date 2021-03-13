05a94b8a827e6f569d211107a04ad884afde4b8d
Daniel Noyola <daniel.noyola.dev@gmail.com>
2021-03-12 22:16:03 -0600

    more styles.

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -73,6 +73,10 @@ export const PostContent = styled.section`
     font-weight: 400;
     box-shadow: inset 0 -2px 0 var(--color-primary);
     transition: all ease-out 0.1s;
+    &::hover,
+    &::focus {
+      box-shadow: inset 0 -3px 0 var(--color-primary-active);
+    }
   }
 
   h1,
@@ -106,12 +110,48 @@ export const PostContent = styled.section`
     margin: 0 0 4rem;
     padding-left: 2em;
     box-sizing: border-box;
+    > li:before {
+      position: absolute;
+      width: 1.5em;
+      margin-left: -2em;
+      font-size: 1em;
+      font-weight: 500;
+      line-height: 2em;
+      text-align: right;
+      color: var(--color-primary);
+      font-family: var(--font-primary);
+    }
+    ol,
+    ul {
+      padding-left: 30px;
+      margin: 15px 0;
+    }
+  }
+  ol {
+    list-style-type: none;
+    counter-reset: item;
+    > li:before {
+      content: counter(item) ". " !important;
+      counter-increment: item;
+    }
   }
   ul {
     list-style: none;
+    > li:before {
+      margin-top: -1px;
+      content: "—";
+    }
   }
   li {
     position: relative;
+  }
+  hr {
+    display: block;
+    height: 1px;
+    margin: 0 0 4rem;
+    padding: 0;
+    background: var(--color-background-contrast);
+    border: 0;
   }
   blockquote {
     font-family: var(--font-secondary);
@@ -132,5 +172,192 @@ export const PostContent = styled.section`
       &:last-child {
       }
     }
+  }
+  em,
+  i {
+    font-style: italic;
+  }
+
+  b,
+  strong {
+    font-weight: 700;
+    color: var(--color-content-lead);
+  }
+
+  small {
+    font-size: 0.9em;
+  }
+
+  dl {
+    font-family: var(--font-secondary);
+    margin: 0 0 4rem;
+    padding-left: 30px;
+    dt {
+      font-weight: 500;
+      font-size: 0.75em;
+      line-height: 1.25em;
+      font-weight: 700;
+      margin-bottom: 0.33334em;
+      font-family: var(--font-primary);
+      ::before {
+        content: "";
+        position: absolute;
+        width: 1em;
+        height: 2px;
+        margin-left: -30px;
+        margin-top: 0.5em;
+        background: var(--color-primary);
+      }
+    }
+    dd {
+      margin-left: 0;
+      margin-bottom: 1em;
+    }
+  }
+
+  code,
+  tt {
+    position: relative;
+    margin: 0 1px;
+    padding: 4px 8px;
+    font-family: monospace, sans-serif;
+    font-size: 0.75em;
+    background: var(--color-background-secondary);
+    border: 1px solid var(--color-background-contrast);
+    top: -2px;
+    white-space: pre-wrap;
+  }
+
+  code[class*="language-"],
+  pre[class*="language-"] {
+    background: none;
+    font-family: Consolas, Menlo, Monaco, source-code-pro, Courier New,
+      monospace;
+    font-feature-settings: normal;
+    text-align: left;
+    white-space: pre;
+    word-spacing: normal;
+    word-break: normal;
+    word-wrap: normal;
+    line-height: 1.5;
+    margin-bottom: 0;
+    -moz-tab-size: 4;
+    -o-tab-size: 4;
+    tab-size: 4;
+    -webkit-hyphens: none;
+    -moz-hyphens: none;
+    -ms-hyphens: none;
+    hyphens: none;
+  }
+  /* Code blocks */
+  pre[class*="language-"] {
+    overflow: auto;
+    padding: 1.3125rem;
+  }
+  pre[class*="language-"]::-moz-selection {
+    /* Firefox */
+    background: hsl(207, 4%, 16%);
+  }
+  pre[class*="language-"]::selection {
+    /* Safari */
+    background: hsl(207, 4%, 16%);
+  }
+  /* Text Selection colour */
+  pre[class*="language-"]::-moz-selection,
+  pre[class*="language-"] ::-moz-selection {
+    text-shadow: none;
+    background: hsla(0, 0%, 100%, 0.15);
+  }
+  pre[class*="language-"]::selection,
+  pre[class*="language-"] ::selection {
+    text-shadow: none;
+    background: hsla(0, 0%, 100%, 0.15);
+  }
+  /* Inline code */
+  :not(pre) > code[class*="language-"] {
+    border-radius: 0.3em;
+    background: var(--inlineCode-bg);
+    color: var(--inlineCode-text);
+    padding: 0.15em 0.2em 0.05em;
+    white-space: normal;
+  }
+  .token.attr-name {
+    color: rgb(173, 219, 103);
+    font-style: italic;
+  }
+  .token.comment {
+    color: rgb(128, 147, 147);
+  }
+  .token.string,
+  .token.url {
+    color: rgb(173, 219, 103);
+  }
+  .token.variable {
+    color: rgb(214, 222, 235);
+  }
+  .token.number {
+    color: rgb(247, 140, 108);
+  }
+  .token.builtin,
+  .token.char,
+  .token.constant,
+  .token.function {
+    color: rgb(130, 170, 255);
+  }
+  .token.punctuation {
+    color: rgb(199, 146, 234);
+  }
+  .token.selector,
+  .token.doctype {
+    color: rgb(199, 146, 234);
+    font-style: "italic";
+  }
+  .token.class-name {
+    color: rgb(255, 203, 139);
+  }
+  .token.tag,
+  .token.operator,
+  .token.keyword {
+    color: #ffa7c4;
+  }
+  .token.boolean {
+    color: rgb(255, 88, 116);
+  }
+  .token.property {
+    color: rgb(128, 203, 196);
+  }
+  .token.namespace {
+    color: rgb(178, 204, 214);
+  }
+  pre[data-line] {
+    padding: 1em 0 1em 3em;
+    position: relative;
+  }
+  .gatsby-highlight-code-line {
+    background-color: hsla(207, 95%, 15%, 1);
+    display: block;
+    margin-right: -1.3125rem;
+    margin-left: -1.3125rem;
+    padding-right: 1em;
+    padding-left: 1.25em;
+    border-left: 0.25em solid #ffa7c4;
+  }
+  .gatsby-highlight {
+    margin-bottom: 1.75rem;
+    margin-left: -1.3125rem;
+    margin-right: -1.3125rem;
+    border-radius: 10px;
+    background: #011627;
+    -webkit-overflow-scrolling: touch;
+    overflow: auto;
+  }
+  @media (max-width: 672px) {
+    .gatsby-highlight {
+      border-radius: 0;
+    }
+  }
+  .gatsby-highlight pre[class*="language-"] {
+    float: left;
+    min-width: 100%;
   }
 `;
